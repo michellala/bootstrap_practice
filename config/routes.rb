2a1f385c81b447501ec43c1a8b7dbcd639306d35
Michelle Alarie <alarie.michelle@gmail.com>
2016-09-11 20:18:51 -0400

    gems for bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+  root 'postss#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  posts = Post.create([{ question: 'Tell us about yourself' }, {answers: 'I am cool'}])
+
+
+# Todo.create!(title: 'grocery shopping', notes: 'p')
+# Todo.create!(title: 'shopping', notes: 'potato')
+
+# ShopStorm.create!(question:'About Me', answers:'Answer to about me')
+# ShopStorm.create!(question:'Tell us about when you had to say no to a customer', answers:'I gave them candy')
+# ShopStorm.create!(question:'Is the customer always right', answers:'YES')
+# ShopStorm.create!(question:'Favorite emoji?', answers: 'Was the religious looking wise man, but now I am unsure')
